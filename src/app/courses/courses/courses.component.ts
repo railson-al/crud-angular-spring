9fbea1af66d913fe1769ac4c84d61ec98dc49487
Railson Almeida <railsonalmeida.dev@gmail.com>
2023-05-28 22:10:19 -0300

    First Commit

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Courses } from '../model/courses';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Courses[] = [
+    {_id: '1', name: 'Angular', category: 'front-end'},
+    {_id: '2', name: 'Spring', category: 'back-end'}
+  ];
+
+  displayedColumns: string[] = ['name', 'category']
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
